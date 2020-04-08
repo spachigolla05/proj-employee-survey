066e90d8858543f066382a7f19648dbc75c2f6cd
spachigolla05 <spachigolla05@gmail.com>
2020-04-08 03:38:22 -0700

    Made changes to test spring config client

---
+++ src/main/java/com/cloudtech/projemployeesurvey/ProjEmployeeSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtech.projemployeesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjEmployeeSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjEmployeeSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudtech/projemployeesurvey/ProjEmployeeSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtech.projemployeesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjEmployeeSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
